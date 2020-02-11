cdba69e4da612db19d8517f69ad980d08a651ee9
A. Z. M. JALAL UDDIN JOY <joy.srijon@gmail.com>
2020-02-11 11:05:18 +0600

    3 (using express & formatting response) social app
    social communication app

--- functions/index.js
+++ functions/index.js
@@ -23,17 +23,8 @@ let defaultAppConfig = {
 }
  
   admin.initializeApp(defaultAppConfig);
-
-
-
-//admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello Bangladesh");
-});
+  const express = require('express');
+  const app = express();
 
 
 exports.getScreams = functions.https.onRequest((req, res) =>{
