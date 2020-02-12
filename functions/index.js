8d5941b0d2eec30801935599b4909cc900ac10d3
A. Z. M. JALAL UDDIN JOY <joy.srijon@gmail.com>
2020-02-12 13:55:51 +0600

    4 successfully got token

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,28 @@
 const functions = require('firebase-functions');
-//const admin = require('firebase-admin');
-
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const app = require('express')();
-admin.initializeApp();
-
-//var serviceAccount = require("path/to/serviceAccountKey.json");
-
 
 let defaultAppConfig = {
     credential: admin.credential.cert({
         "type": "service_account",
         "project_id": "socialapp-2e25e",
-        "private_key_id": "4f57a57b7851b996e8e15a1dd3f498a736804a50",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvy9JALEfVm0d5\nqDgxCY31lBtvFaZhAIXIaVJhPxy83Wv6dbM8qWydkTQinZfIOI/LhQDTkGT8ktuE\nX+71TijDBlM8S4GT2NV68YMZe4a9Do7xVEiweurweYgJWxNaFFncS7DAljMdpctz\nf++uyEtCT6KezepcsVhk+KdddZOiPQYm2jKPQ1lx3iAoyTghDM10F6bkV8FyhWMs\nbTEdrpsPIeiOMFA7OH3cy67qgIkuaSriFree+f0uJ4GEPIcSs6tjb2lchSKzbuMF\nPhFNXHSrTCq8IBngSgmsCeyOV9X+ufXYvXH2JLaZr76PoH/xPjolUQD8iwEbuIt2\nK0Jw/I51AgMBAAECggEABaHiY993Urr1pIp0+G89ruFWqq23JbL6VlQ3iTmYLl3q\nxHzKaBZmjjrVgIlrkETtqH/jqaOd9LCw21ImihH9wNfeSVeC+I1xM43y3Rll1P9X\nKZjBdnQNvHQsXV5fZZqy0VnUeSsmypuiA9qUv5UXwjFifLOMv3kII74KwLYQa5l6\nz/hoS0rtWwM0pvxcyOqTMdtwu8BOSydvtH3vJLZ5WD3xSdDtvDKO7GkBKE381V4u\nkenjythdgnFaLrseg92J2BUO1egAEzBTe2bm5z3WnwLxCYq7a6Y3bftgKFX2zUzj\nIRKEungAJ6gfbs4x0T4iiJLeXBknyViHhEID0+u6sQKBgQDgrCrCTFUOx9MfIWyE\nkIf6sO4goXnR6zuf67w20Mylz9Jvuz6uusynZE8RS70NGz+yEIIOupYL54Xag27h\na2CyKG5fJx4/BgRGgi0bIimpZi/YmzCSdYka+SwipMxNMdg0Mz/RqVeWe94uLZBm\nLokdN0dc4NGfRGbVqIOAQ/aa0QKBgQDITvsePxA6ZTZLtiGwooysbVCHD5CKvJG+\nJA3FtGiE9rvDWJoWKbs5E6NJTkd51kLuH7ST1RnNhBh6Vw4NKjp5BIShmzGshDB3\nLUea7NZ0+CwDEOaiKYpk93g+UcVt4oeqq2qR8hC+vqhAv3+cfwSCfl8dVKDMjQBY\nh9J7EwRaZQKBgQCfGSw6ZSWCjvyxOdEH2vWAEBTA5LhIb+TMLXod8yRIcu+ZEaak\nw0EzdHQNUm3wvxAO/2OqHHUuvufpwlMdrBVQdBMcrK3dgVw7telNIpcQD33oTJVL\n0ZGimTqo2byXGan55STXSqzsspC2cKulwGR2XPPbEJVAzIZp3Kgqh3Y1UQKBgQCi\nZx7EnB69KbhG1EkmW/QwDIUgq+bmBRMpFTQxWh7vxtfe/uQXGgiYsypNUfEzgBtN\n01jmDv3XmcT608ApvlYgLiCWoiw5Egf1aDnJC0KpHVkZt0c2rPVu/RF5fXTTrbmP\nD2HdKGSZMh1aOajuKjFu16SoNCgXEC76mEx3yqhZ3QKBgA6RtR4SzZTzKUiJruIF\nGt8AD4Bmz/xLOo6xEJQF/L3QYNgLHNINv1DHUDCVBtVDQKdJOf+ctrU77EfgeYd/\nljDdACjKRBddPHLwSTCFASE7z7xb27NZEmhJwvny6EppsRI4q5LD+Ng1tTcFDyPF\nQ3NX1g8BiPfcjvcAUHk0PA+m\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-zrhrv@socialapp-2e25e.iam.gserviceaccount.com",
-        "client_id": "101887291894340379013",
+        "private_key_id": "de34bbb366e3c052ec319430ee6e9d694c013a4b",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCjduvRYPy2hmDY\nu0rLIG03ij7rMMt7mQTjJ7DJ12gF17QILTW9VcM3gXPMZ/RXx3Yv5txyBnJouu62\n+gkLTeSf25NjIZGnOsANa1Z8KFfy+2W9XsaOwrORyUrJabj+3DQPPlOA4DWy5Lcc\n7pIGzcV8neRonP8//w0nyZge+W8kbSs2aaa9BbdY21nieCAx2GdQSiaHcoTOVcD1\n6jpfeNQahHe8gMpaWZYB9L7PryRLaWO0zvb/BCZjTkaWZyhQT+M/KdUzw/z0DdUr\nnAdI5iu5zFH2YRg4EveK+IT17E0EWE4jBO5Xng+3vL987/Bj8KjPw/OSEkcTvxyQ\n/RFDHGZ7AgMBAAECggEAEwHDPnpSMtWshybn9cx/G+PQQxVOY8Bm0r7iYzslFkt+\n3ICH7MxUbFd6DnzRQC+qLLxhn5njow0xWpyiK+qnuHVrTYMxZh+CW00YJkxOmwCO\nU5b57CRJhZxr52rf9DUd5zPZDnWDk5ndN4Op/m6IwF+07PvBq5Wi2EssNqshjZDl\nySAZkWunLqgOkDyBBjbJ2SSj077yBi2XLLECcIDFLtJ1Q4z1qogfeXTw6PiWAWZH\nLKxlNjbwdXCRmKRwkFg7umOdqMzQGKwyhBx57doMuyQKDGk6cSkjuQUMVt5xe0nV\naW4Imk44nB4Cket94qUNIAlyLSzu+7sG1qouX3mVoQKBgQDT0ZQ/f1hivVzu72qS\nJ6x4L/fgnYYlWoptgK+T54g2RJQh4OVP4aCBUhyVGXNlBnxK3C1KthgGm+/+fwCY\n2Wbqq1CjMAJrjs344jEtuH/p1f2b8avPbN4Br80Kw5fuqy339Holh4a9C/MhIHkj\nfqGkRvz4lcv272kmibpBYRJxcQKBgQDFj2Po13EtKEHZ21sA3+ew0oAhX2By8a9l\ns401pDuK3lxTuttHKBHwBvaCOYO6uSoKUA0q4aMZ3VlowqnPMGFFFaJYa+H3skKb\n4EdRIF/PYf7YZ2NGOkAM6F5wZqpnLnH4RtxT9dtnmaMS8nMRFm/Y29Nemvh4OzO1\nlRh4gSSgqwKBgFtcOvAEUjAMcwgPtcyiVg5kyipUZya3XZuLdK97ntGhVK8kHGQO\n8ja1HuTkXFRHrtZvDw35anV3TtVbG/vRUUWIbLhmHbUcnBRsI0AQIwPNq5xWaX+G\n+l6J4Atzf5WOub4H8aM8SE5D2DMCmZ1IohmEENp7acTxUE4x+2b1oonRAoGBAL54\nVZn6hKpF4t84xk9ckBOBj3kdbG7FpxZFewJZmfJ0Gc28HIdJEao5/FZnPyK6PNb+\ngQNz0X0xVG9VJsST32PszL21HYJjAicon3mO3fCiiUJ4w1TE+lW3fr2drfKW69iz\n8N6d7e+/R2MQD3JNEH6iyfGvNZktcthBq/saf4bLAoGBAKJT+pLMyfqVt/EBYRxU\nLYqFYceMalOJI70Y0/a5zl6P+P5lVT1kNRXR5Mk8S3QhAww/hW53DJRDAllBP9+i\n4dSUN24sRH15H2O1XXGYRPiGOEBPCvw7lAuY5f+4a5tTPfpf0CWornAP/MnMeX4C\n6wuucCvPmdayNfp3azCWxoHM\n-----END PRIVATE KEY-----\n",
+        "client_email": "socialapp-2e25e@appspot.gserviceaccount.com",
+        "client_id": "110381063559436087465",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zrhrv%40socialapp-2e25e.iam.gserviceaccount.com"
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/socialapp-2e25e%40appspot.gserviceaccount.com"
     }),
     databaseURL: 'https://socialapp-2e25e.firebaseio.com/'
 }
+
+admin.initializeApp(defaultAppConfig);
+
+//var serviceAccount = require("path/to/serviceAccountKey.json");
+ 
+ 
 
   const config = {
     apiKey: "AIzaSyAySVV2P_A4ACZkJdbsxMMx-vTtiYkIgAY",
@@ -33,6 +33,7 @@ let defaultAppConfig = {
     messagingSenderId: "387325985079",
     appId: "1:387325985079:web:d4598f22851ebd1b3edab2",
     measurementId: "G-H90077G5KV"
+ 
   };
   const firebase =  require('firebase');
   firebase.initializeApp(config);
@@ -82,40 +83,92 @@ app.post('/scream', (req, res) => {
 });
 
 //signup route
+
 app.post('/signup', (req, res) => {
     const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      handle: req.body.handle
     };
 
-    // TODO: validate data
-    db.doc(`/users/${newUser.handle}`).get()
-    .then(doc => {
-        if(doc.exists){
-            return res.status(400).json({ handle: 'this handle is already taken' });
-        } else {
-            return firebase
-            .auth()
-            .createUserWithEmailAndPassword(newUser.email, newUser.password);
-        }
+    db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
     })
     .then((data) => {
+       
         return data.user.getIdToken();
-    })
-    .then((token) =>{
+      })
+ 
+    .then((token) => {
         return res.status(201).json({ token });
     })
     .catch((err) =>{
         console.error(err);
-        if(err.code === 'auth/email-already-in-use'){
-            return res.status(400).json({ email: 'Email is already in use'});
-        } else {
-            return res.status(500).json({ error: err.code });
-        }
-    })
-});
+        return res.status(500).json({ error: err.code });
+        })
+    });
+
+
+    // firebase
+    //     .auth()
+    //     .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    //     .then(data => {
+    //      return res
+    //         .status(201)
+    //         .json({ message: `user ${data.user.uid} signed up successfully` });
+    //     })
+    //     .catch((err) => {
+    //         console.error(err);
+    //         return res
+    //         .status(500)
+    //         .json({error: err.code});
+    //     });
+
+    // db.doc(`/users/${newUser.handle}`).get()
+    // .then(doc => {
+    //     if(doc.exists){
+    //         return res.status(400).json({ handle: 'this handle is already taken' });
+    //     } else {
+    //         return firebase
+    //         .auth()
+    //         .createUserWithEmailAndPassword(newUser.email, newUser.password);
+    //     }
+    // })
+    // .then((data) => {
+    //     userId = data.user.uid;
+    //     return data.user.getIdToken();
+    // })
+    // .then((token) =>{
+    //     token = token;
+    //     const userCredentials = {
+    //         handle: newUser.handle,
+    //         email: newUser.email,
+    //         createdAt: new Date().toISOString(),
+    //         userId
+    //     };
+    //     return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    // })
+    // .then(() => {
+    //     return res.status(201).json({ token });
+
+    // })
+    // .catch((err) =>{
+    //     console.error(err);
+    //     if(err.code === 'auth/email-already-in-use'){
+    //         return res.status(400).json({ email: 'Email is already in use'});
+    //     } else {
+    //         return res.status(500).json({ error: err.code });
+    //     }
+    // })
 
 exports.api = functions.https.onRequest(app);
 
