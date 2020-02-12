04907b9b40a8b05212b615e07ee723b2b09928aa
A. Z. M. JALAL UDDIN JOY <joy.srijon@gmail.com>
2020-02-12 15:23:46 +0600

    5 user validation (signup route)

--- functions/index.js
+++ functions/index.js
@@ -82,6 +82,18 @@ app.post('/scream', (req, res) => {
         });
 });
 
+
+const isEmail = (email) => {
+    const RegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(RegEx)) return true;
+    else return false;
+}
+
+const isEmpty = (String) => {
+    if(String.trim() === '') return true;
+    else return false;
+}
+
 //signup route
 
 app.post('/signup', (req, res) => {
@@ -91,6 +103,19 @@ app.post('/signup', (req, res) => {
       confirmPassword: req.body.confirmPassword,
       handle: req.body.handle
     };
+
+    let errors = {};
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     //Validate data
     let token, userId;
@@ -131,59 +156,6 @@ app.post('/signup', (req, res) => {
         }
         })
     });
-
-
-    // firebase
-    //     .auth()
-    //     .createUserWithEmailAndPassword(newUser.email, newUser.password)
-    //     .then(data => {
-    //      return res
-    //         .status(201)
-    //         .json({ message: `user ${data.user.uid} signed up successfully` });
-    //     })
-    //     .catch((err) => {
-    //         console.error(err);
-    //         return res
-    //         .status(500)
-    //         .json({error: err.code});
-    //     });
-
-    // db.doc(`/users/${newUser.handle}`).get()
-    // .then(doc => {
-    //     if(doc.exists){
-    //         return res.status(400).json({ handle: 'this handle is already taken' });
-    //     } else {
-    //         return firebase
-    //         .auth()
-    //         .createUserWithEmailAndPassword(newUser.email, newUser.password);
-    //     }
-    // })
-    // .then((data) => {
-    //     userId = data.user.uid;
-    //     return data.user.getIdToken();
-    // })
-    // .then((token) =>{
-    //     token = token;
-    //     const userCredentials = {
-    //         handle: newUser.handle,
-    //         email: newUser.email,
-    //         createdAt: new Date().toISOString(),
-    //         userId
-    //     };
-    //     return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    // })
-    // .then(() => {
-    //     return res.status(201).json({ token });
-
-    // })
-    // .catch((err) =>{
-    //     console.error(err);
-    //     if(err.code === 'auth/email-already-in-use'){
-    //         return res.status(400).json({ email: 'Email is already in use'});
-    //     } else {
-    //         return res.status(500).json({ error: err.code });
-    //     }
-    // })
 
 exports.api = functions.https.onRequest(app);
 
