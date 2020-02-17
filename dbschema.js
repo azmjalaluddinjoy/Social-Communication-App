61d9e8634a9c44e31c5c3890053e1cfd9661d321
A. Z. M. JALAL UDDIN JOY <joy.srijon@gmail.com>
2020-02-17 13:01:50 +0600

    10 getting comment from database of an user

--- dbschema.js
+++ dbschema.js
@@ -12,16 +12,25 @@ users: [
     }
 ],
 
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream body',
-            createdAt: '2020-02-11T06:46:57.040Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ]
+screams: [
+    {
+        userHandle: 'user',
+        body: 'this is the scream body',
+        createdAt: '2020-02-11T06:46:57.040Z',
+        likeCount: 5,
+        commentCount: 2
+    }
+],
+comments = [
+    {
+        userHandle: 'user',
+        screamId: 'P1Zz0mz3Gxsf4oPb6R66',
+        body: 'nice to meet you bro !',
+        createdAt: '2020-02-17T06:51:02.211Z'
+    }
+],
 };
+
 
 const userDetails = {
     //redux code
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require('firebase-functions');
-
 const app = require('express')();
-
 const FBAuth = require('./util/fbAuth');
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream} = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 //j
@@ -13,13 +11,19 @@ const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = req
   //scream routes
   app.get('/screams', getAllScreams);
   app.post('/scream', FBAuth, postOneScream);
-  app.post('/user/image', FBAuth, uploadImage);
-  app.post('/user', FBAuth, addUserDetails);
-  app.get('/user', FBAuth, getAuthenticatedUser);
+  app.get('/scream/:screamId', getScream);
+  // TODO: delete scream
+  // TODO: like a scream
+  // TODO: unlike a scream
+  // TODO: comment on scream
+
 
   // users routes
   app.post('/signup', signup);
   app.post('/login', login);
+  app.post('/user/image', FBAuth, uploadImage);
+  app.post('/user', FBAuth, addUserDetails);
+  app.get('/user', FBAuth, getAuthenticatedUser);
   
 
 //link
