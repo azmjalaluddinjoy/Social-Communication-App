5a2bb669e398dacbc5bd0914e72a56257c5401b1
A. Z. M. JALAL UDDIN JOY <joy.srijon@gmail.com>
2020-02-18 10:28:55 +0600

    11 Like-unLike and Delete Scream completed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-const {getAllScreams,postOneScream, getScream,commentOnScream, likeScream, unlikeScream}= require('./handlers/screams');
+const {getAllScreams,postOneScream, getScream,commentOnScream, likeScream, unlikeScream, deleteScream}= require('./handlers/screams');
 const { 
   signup, 
   login, 
@@ -21,6 +21,7 @@ const {
   app.get('/scream/:screamId', getScream);
   
   // TODO: delete scream
+  app.delete('/scream/:screamId', FBAuth, deleteScream);
   app.get('/scream/:screamId/like', FBAuth, likeScream);
   app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
   app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
